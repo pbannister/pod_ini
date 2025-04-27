cd20ccdf100561d26d9db78247ec94c92e4653a5
Preston L. Bannister <preston@bannister.us>
2025-04-27 14:47:11 -0700

    WIP - scripts.

--- tests/config-gen.sh
+++ tests/config-gen.sh
@@ -1,6 +1,12 @@
 
 #!/bin/sh
 
+FILE1=in/example1.ini
+FILE2=out/example2.ini
+
+TMP1=/tmp/_file1
+TMP2=/tmp/_file2
+
 BIN=../build/bin
 
 test -d tests && cd tests
@@ -10,8 +16,13 @@ test -d in || {
 }
 
 set -x
-$BIN/pod_racer -d in/example1.ini > out/config-declare.h
-$BIN/pod_racer -D in/example1.ini > out/config-initialize.h
-$BIN/pod_racer -R in/example1.ini > out/config-reader.cpp
+$BIN/pod_racer -d $FILE1 > out/config-declare.h
+$BIN/pod_racer -D $FILE1 > out/config-initialize.h
+$BIN/pod_racer -R $FILE1 > out/config-reader.cpp
+
+$BIN/pod_racer -i $FILE1 > $FILE2
+sh ini-normalize.sh $FILE1 > $TMP1
+sh ini-normalize.sh $FILE2 > $TMP2
+diff -ub $TMP1 $TMP2
 
 
---
+++ tests/ini-normalize.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+#
+
+cat $* | sed -e 's, *= *,=,' | awk -F= '
+BEGIN {
+	kv[0]
+}
+/^[#;]/ {
+	next
+}
+/^\[/ {
+	gsub(/[\]\[]/,"")
+	sub(/ *$/,"")
+	k1 = $0
+	next
+}
+/=/ {
+	k2 = $1
+	kk = k1 "_" k2
+	gsub(/[- .:/,$]+/,"_", kk)
+	kv[kk] = $2
+	# print "## " $0
+	# print "## " kk "=" $2
+}
+END {
+	for (k in kv) {
+		v = kv[k]
+		print k "=" v
+	}
+}' | sort -u
+
